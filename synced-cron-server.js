38209cba7e327a5e7e0065597f45e95d641c8bff
Diogo Lima <diogo.lima@controllerti.com.br>
2023-03-09 09:19:49 -0300

    Remove milliseconds to avoid duplication

--- synced-cron-server.js
+++ synced-cron-server.js
@@ -266,6 +266,10 @@ SyncedCron._entryWrapper = function(entry) {
   var self = this;
 
   return function(intendedAt) {
+    intendedAt = new Date(intendedAt.getTime());
+    intendedAt.setMilliseconds(0);
+    intendedAt.setSeconds(0);
+
     var jobHistory = {
       intendedAt: intendedAt,
       name: entry.name,
